66aa6b997195eecf0b601f2e2dd49a6fb487924a
lucasvaltl <lvaltl@online.de>
2017-02-25 17:05:07 +0000

    first draft and initial commit

---
+++ circles-overview.php
@@ -0,0 +1,92 @@
+<?php
+
+//ob_start needed to allow redirecting after login
+ob_start();
+
+//session_start() needed to use global session variabls $_SESSION etc
+session_start();
+
+include('includes/config.php');
+require_once('includes/dbconnect.php');
+$filename = basename(__FILE__, '.php');
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+  <script src="https://use.fontawesome.com/ed51c90fe4.js"></script>
+  <link rel="stylesheet" href="css/offset.css">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Pano - Profile</title>
+</head>
+
+<body>
+  <?php
+  include('includes/header.php');
+  ?>
+  <main>
+    <div class="profile-header">
+      <?php
+      if (isset($_GET['id'])) {
+        $profileUserName = $_GET['id'];
+      }
+      ?>
+    </div>
+    <div class="content circles-overview container">
+
+      <div class="circles-overview-header"></div>
+      <h2>Your  Circles</h2>
+      <br />
+
+      <div class="circles-overview-content">
+
+
+        <?php
+        include('includes/circle-cover.php');
+
+        //create an array of collections - will need to be redone with php when the database is ready
+        $collections = [
+          new circle('circle.php', 'IMG_8937', 'Besties'),
+          new circle('circle.php', 'IMG_2821', 'London Crew'),
+          new circle('circle.php', 'IMG_6346', 'MSCCSUCL')
+        ];
+
+        $count = 1;
+        echo '<hr/> ';
+        //insert the collections into the page
+        foreach($collections as $collection){
+
+          // insert a new row every two elements
+          if($count % 2 != 0){
+            echo '<div class="row"> ';
+            $collection->setBorderRight();
+          }
+          //insert post
+          $collection->returnHTML();
+          //close row every two elements and insert a dividor
+          if($count % 2 == 0){
+            echo '</div> <hr/> ';
+          }
+          $count += 1;
+
+        }
+
+        ?>
+      </div>
+
+    </div>
+
+
+
+  </main>
+  <?php
+  include('includes/footer.php');
+  ?>
+
+</body>
+
+</html>
--- includes/footer.php
+++ includes/footer.php
@@ -7,7 +7,7 @@
       <a href="<?=SITE_ROOT?>/home.php">     <i class="fa fa-newspaper-o fa-3x"></i></a>
     </div>
     <div class="col-md-15  col-xs-15  lv-icons">
-        <a href="<?=SITE_ROOT?>/circles.php">   <i class="fa fa-circle-thin fa-3x"></i></a>
+        <a href="<?=SITE_ROOT?>/circles-overview.php">   <i class="fa fa-circle-thin fa-3x"></i></a>
     </div>
     <div class="col-md-15  col-xs-15  lv-icons">
         <a href="<?=SITE_ROOT?>/upload.php">   <i class="fa fa-camera fa-3x"></i></a>
