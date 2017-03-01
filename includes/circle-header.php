e8e47522785f47201cc4670f79c73d174e94d8bf
lucasvaltl <lvaltl@online.de>
2017-03-01 18:38:52 +0000

    minor update

--- includes/circle-header.php
+++ includes/circle-header.php
@@ -74,7 +74,7 @@ $isPartOfCircle = false;
 <div class="container profile-info">
     <div class="row ">
         <div class="col col-md-3 col-xs-3 profile-info-row">
-            <img src="<?=SITE_ROOT?>/images/panoramas/<?=$circleProfilePictureID?>.jpg" class="img-circle img-responsive profile-user-picture " />
+            <img src="<?=SITE_ROOT?>/images/panoramas/<?=$circleProfilePictureID?>.jpg" class="img-responsive circle-cover " />
         </div>
         <div class="col col-md-6  col-xs-6 container">
             <p class="profile-info-name">
