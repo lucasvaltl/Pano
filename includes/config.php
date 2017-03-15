681647c4bdc335392c5c04d9d5d8bf1a9f53e4d7
Li Xie <li_xie1992@hotmail.com>
2017-03-15 20:21:35 +0000

    new config option

--- includes/config.php
+++ includes/config.php
@@ -2,10 +2,15 @@
 // Please select and make corresponding change in dbconnect.php
 // Option 1: Connection to Azure /////////////////////////////////
 
-define ('SITE_ROOT', 'http://app-pano.azurewebsites.net');
+//define ('SITE_ROOT', 'http://app-pano.azurewebsites.net');
 
 // OPTION 2: Connection to local DB /////////////////////////////////
 
 //define ('SITE_ROOT', '/panoapp');
+
+
+// OPTION 3: Connection to www.panoapp.co.uk ////////////////////////////////
+
+define ('SITE_ROOT', 'http://panoapp.co.uk');
 
 ?>
