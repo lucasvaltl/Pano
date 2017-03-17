6e8fc10aea09b854dc2ab7cca57e47924accb12e
lucasvaltl <lvaltl@online.de>
2017-03-17 02:56:43 +0000

    ehm ups

--- upload.php
+++ upload.php
@@ -13,7 +13,7 @@ if (isset($_SESSION['UserID'])) {
   $real_name = $UserID . time();
   $blob_name = hash('sha256',$real_name);
 }
-(!isset($_SESSION['uploadSuccessful'])) {
+if (!isset($_SESSION['uploadSuccessful'])) {
 $_SESSION['uploadSuccessful'] = false;
 }
 
